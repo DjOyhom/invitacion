561ba8c6535ecc5512e944a9de67dfe5616ef473
rodrigo <rodrigo19-1_1998@hotmail.com>
2020-01-28 22:13:12 -0300

    first

---
+++ js/mapita.js
@@ -0,0 +1,26 @@
+function mapsSelector() {
+    if ((navigator.platform.indexOf("iPhone") != -1) || (navigator.platform.indexOf("iPad") != -1) || (navigator.platform.indexOf("iPod") != -1)) { /* si es de cheto */
+      window.open("maps://maps.google.com/maps?saddr=My+Location&daddr=Oficina+del+Registro+de+Estado+Civil+de+la+Republica+Oriental+del+Uruguay");
+    }
+    else { /* else use Google */
+      window.open("https://maps.app.goo.gl/QrxX7BZ9GjXV5xEM6");
+    }
+  }
+
+  function mapsSelector2() {
+    if ((navigator.platform.indexOf("iPhone") != -1) || (navigator.platform.indexOf("iPad") != -1) || (navigator.platform.indexOf("iPod") != -1)) { /* si es de cheto */
+      window.open("maps://maps.google.com/maps?saddr=My+Location&daddr=Parroquia+Inmaculada+Concepción,+Paraguay+2102,+12700+Montevideo,+Departamento+de+Montevideo");
+    }
+    else { /* else use Google */
+      window.open("https://maps.app.goo.gl/pAte6ZQGrYuvszNk9");
+    }
+  }
+
+  function mapsSelector3() {
+    if ((navigator.platform.indexOf("iPhone") != -1) || (navigator.platform.indexOf("iPad") != -1) || (navigator.platform.indexOf("iPod") != -1)) { /* si es de cheto */
+      window.open("maps://maps.google.com/maps?saddr=My+Location&daddr=Chacra+Los+Alamos,+Cno.+Manuel+las+flores+9097,+12700+Montevideo");
+    }
+    else { /* else use Google */
+      window.open("https://maps.app.goo.gl/DrSz5sxj7imC4Dc66");
+    }
+  }
